ec7de1fb80f647db02a55b5ea376090e6b9c6b70
ksp3009 <kaushik.sp2002@gmail.com>
2026-06-20 23:18:05 -0500

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return max(nums)
+
+        dp_left = [0]*(n-1)
+        dp_right = [0]*(n-1)
+        dp_left[0] = nums[0]
+        dp_left[1] = max(nums[0], nums[1])
+        
+        reversed_nums = nums[::-1]
+        dp_right[0] = (reversed_nums[0])
+        dp_right[1] =  max(reversed_nums[0], reversed_nums[1])
+        for i in range(2, len(nums)-1):
+            dp_left[i] = max(dp_left[i-1], dp_left[i-2] + nums[i])
+
+        for j in range(2, len(nums)-1):
+            dp_right[j] = max(dp_right[j-1], dp_right[j-2] + reversed_nums[j])
+            
+
+        return max(dp_left[-1], dp_right[-1])
+
+# (res[i] = res[i-1], res[i-2]+nums[i])
